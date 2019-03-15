9855760c89159e9a2674bf83f92a640d1bc3977e
raketenlurch <annkatrin.le@gmx.de>
2019-03-15 06:07:23 +0100

    Add GUI-Tutorial

---
+++ src/tutorial_eins.java
@@ -0,0 +1,11 @@
+public class tutorial_eins {
+
+    /**
+     * @param args
+     * */
+    public static void main(String[] args) {
+        AppWindow theAppWindow = new AppWindow();
+        theAppWindow.setBounds(10, 10, 420, 180);
+        theAppWindow.show();
+    }
+}
