227e1a3d026ccf7bfa6ae938c3172aa1197c6494
raketenlurch <annkatrin.le@gmx.de>
2019-03-16 20:57:52 +0100

    Add SchaltjahrGUI

---
+++ src/SchaltjahrApp.java
@@ -0,0 +1,10 @@
+public class SchaltjahrApp {
+    /**
+    * @param args
+    **/
+    public static void main(String[] args) {
+        SchaltjahrGUI theApp = new SchaltjahrGUI();
+        theApp.setBounds(10, 10, 420, 180);
+        theApp.show();
+    }
+}
